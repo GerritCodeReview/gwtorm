a5e6f53da91db6ee6873e2dece0f5e697a97b278
Adrian Goerler <adrian.goerler@sap.com>
2011-08-10 16:41:30 +0200

    Fix UPSERT for JDBC batch returning SUCCESS_NO_INFO
    On some databases (such as Oracle or MaxDB), the update count array
    returned by the Statement.executeBatch may contain the general success
    indicator SUCCES_NO_INFO. On such databases a batch UPDATE cannot be
    used to check for existence of a row on the database: the situation that
    a row can't be updated because it is not existing will also yield
    SUCCESS_NO_INFO. For the databases in question, individual updates are
    performed with this patch.
    
    Some databases (such as MaxDB) allow to efficiently determine the
    overall update count of a batch execution. This patch allows to leverage
    this capability in a database-dialect.
    
    Change-Id: Id7cb496fc517e0c31eef2f3c34f566aaac20019d
    Signed-off-by: Adrian Goerler <adrian.goerler@sap.com>

--- src/main/java/com/google/gwtorm/jdbc/JdbcAccess.java
+++ src/main/java/com/google/gwtorm/jdbc/JdbcAccess.java
@@ -27,6 +27,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 /** Internal base class for implementations of {@link Access}. */
 public abstract class JdbcAccess<T, K extends Key<?>> extends
@@ -199,6 +200,11 @@ public abstract class JdbcAccess<T, K extends Key<?>> extends
 
   @Override
   public void upsert(final Iterable<T> instances) throws OrmException {
+    if (!schema.getDialect().hasInfoOnBatchingSuccess()) {
+      upsertIndividually(instances);
+      return;
+    }
+
     // Assume update first, it will cheaply tell us if the row is missing.
     //
     Collection<T> inserts = null;
@@ -249,6 +255,40 @@ public abstract class JdbcAccess<T, K extends Key<?>> extends
     }
   }
 
+  private void upsertIndividually(Iterable<T> instances)
+      throws OrmException {
+    if (!instances.iterator().hasNext()) {
+      return;
+    }
+
+    try {
+      @SuppressWarnings("unchecked")
+      List<T> inserts = Collections.EMPTY_LIST;
+      PreparedStatement ps =
+          schema.getConnection().prepareStatement(getUpdateOneSql());
+      try {
+        for (T o : instances) {
+          bindOneUpdate(ps, o);
+          int updateCount = ps.executeUpdate();
+          if (updateCount == 0) {
+            if (inserts == Collections.EMPTY_LIST) {
+              inserts = new ArrayList<T>();
+            }
+            inserts.add(o);
+          }
+        }
+      } finally {
+        ps.close();
+      }
+
+      insert(inserts);
+
+    } catch (SQLException e) {
+      throw convertError("update", e);
+    }
+  }
+
+
   @Override
   public void delete(final Iterable<T> instances) throws OrmException {
     try {
@@ -274,20 +314,19 @@ public abstract class JdbcAccess<T, K extends Key<?>> extends
     }
   }
 
-  private static void execute(final PreparedStatement ps, final int cnt)
+  private void execute(final PreparedStatement ps, final int cnt)
       throws SQLException, OrmConcurrencyException {
     if (cnt == 0) {
       return;
     }
 
-    final int[] states = ps.executeBatch();
-    if (states == null) {
+    final int[] updateCounts = ps.executeBatch();
+    if (updateCounts == null) {
       throw new SQLException("No rows affected; expected " + cnt + " rows");
     }
-    for (int i = 0; i < cnt; i++) {
-      if (states.length <= i || states[i] != 1) {
+    final int updateCount = ps.getUpdateCount();
+    if(!schema.getDialect().allRowsUpdated(updateCounts, updateCount, cnt)) {
         throw new OrmConcurrencyException();
-      }
     }
   }
 
