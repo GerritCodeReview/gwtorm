901ec4a468263f128b20273967404c675e678515
Johan Martinsson <martinsson.johan@gmail.com>
2012-11-12 10:07:33 +0100

    fixed mysql implementation with respect to throwing OrmDuplicateKeyException
    Change-Id: I19a31a6a03d9079c533e93b0a04f6c7ee96f1553

--- src/main/java/com/google/gwtorm/schema/sql/DialectMySQL.java
+++ src/main/java/com/google/gwtorm/schema/sql/DialectMySQL.java
@@ -17,6 +17,7 @@ package com.google.gwtorm.schema.sql;
 import com.google.gwtorm.client.Column;
 import com.google.gwtorm.schema.ColumnModel;
 import com.google.gwtorm.schema.SequenceModel;
+import com.google.gwtorm.server.OrmDuplicateKeyException;
 import com.google.gwtorm.server.OrmException;
 import com.google.gwtorm.server.StatementExecutor;
 
@@ -202,5 +203,15 @@ public class DialectMySQL extends SqlDialect {
     r.append(" ");
     r.append(getSqlTypeInfo(col).getSqlType(col, this));
     stmt.execute(r.toString());
+  }
+
+  @Override
+  public OrmException convertError(String op, String entity, SQLException err) {
+    switch (err.getErrorCode()) {
+      case 1062:
+        return new OrmDuplicateKeyException(entity, err);
+      default:
+        return fallbackConvertError(op, entity, err);
+    }
   }
 }
