a687fb2c7bba7e5e8c5f192fc00f3204b6b1a116
Saša Živkov <sasa.zivkov@sap.com>
2014-05-14 12:54:13 +0200

    Fix a bug where "LIMIT ?" in queries was omitted in the generated query
    The LIMIT clause was only honored when followed by a constant integer.  When
    followed by a placeholder "?" it wasn't included in the generated database
    query.
    
    This could cause performance issues when selecting a few rows from a large
    table like, for example, when paging through merged changes in Gerrit UI. In
    this case, for every page, most of the closed changes would be selected from
    the Changes table in order to display the next 26 changes. On one of our
    productive Gerrit servers, running on the 2.7 release, it took often more than
    20 seconds to show a next page. With this fix the time is reduced to several
    milliseconds.
    
    (cherry picked from commit b1ffb444674202ddcab8d10ceca5759b96d2a14f)
    
    Change-Id: Ie18898242d9c590eeb4f3eae895d955cd04e1d66

---
+++ src/test/java/com/google/gwtorm/schema/QueryModelTest.java
@@ -0,0 +1,64 @@
+// Copyright 2014 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gwtorm.schema;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.google.gwtorm.data.PhoneBookDb;
+import com.google.gwtorm.schema.java.JavaSchemaModel;
+import com.google.gwtorm.schema.sql.DialectH2;
+import com.google.gwtorm.server.OrmException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class QueryModelTest {
+
+  private RelationModel people;
+
+  @Before
+  public void setUp() throws OrmException {
+    JavaSchemaModel schema = new JavaSchemaModel(PhoneBookDb.class);
+    people = schema.getRelation("people");
+  }
+
+  @Test
+  public void testLimitWithConstant() throws OrmException {
+    QueryModel qm = new QueryModel(people, null, "LIMIT 5");
+    List<ColumnModel> params = qm.getParameters();
+    assertNotNull(params);
+    assertTrue(params.size() == 0);
+    assertTrue(qm.hasLimit());
+
+    String sql = qm.getSelectSql(new DialectH2(), "T");
+    assertEquals("SELECT T.age,T.registered,T.name FROM people T LIMIT 5", sql);
+  }
+
+  @Test
+  public void testLimitWithPlaceholder() throws OrmException {
+    QueryModel qm = new QueryModel(people, null, "LIMIT ?");
+    List<ColumnModel> params = qm.getParameters();
+    assertNotNull(params);
+    assertTrue(params.size() == 0);
+    assertTrue(qm.hasLimit());
+
+    String sql = qm.getSelectSql(new DialectH2(), "T");
+    assertEquals("SELECT T.age,T.registered,T.name FROM people T LIMIT ?", sql);
+  }
+}
