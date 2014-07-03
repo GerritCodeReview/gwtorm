810477b51139105b81f8252d4a24902253612e9d
David Ostrovsky <david@ostrovsky.org>
2014-07-03 22:20:32 +0200

    Factor out transaction unit tests to base class
    Change-Id: I24f36ba55cb5ab2db2c6cb977aad4d27463b0d70

--- src/test/java/com/google/gwtorm/schema/sql/DialectMySQLTest.java
+++ src/test/java/com/google/gwtorm/schema/sql/DialectMySQLTest.java
@@ -223,43 +223,4 @@ public class DialectMySQLTest extends SqlDialectTest {
     assertTrue(s.contains("bar"));
     assertFalse(s.contains("for"));
   }
-
-  @Test
-  public void testRollbackTransaction() throws SQLException, OrmException {
-    PhoneBookDb schema = phoneBook.open();
-    schema.updateSchema(executor);
-    schema.people().beginTransaction(null);
-    ArrayList<Person> all = new ArrayList<>();
-    all.add(new Person(new Person.Key("Bob"), 18));
-    schema.people().insert(all);
-    schema.rollback();
-    List<Person> r = schema.people().olderThan(10).toList();
-    assertEquals(0, r.size());
-  }
-
-  @Test
-  public void testRollbackNoTransaction() throws SQLException, OrmException {
-    PhoneBookDb schema = phoneBook.open();
-    schema.updateSchema(executor);
-    ArrayList<Person> all = new ArrayList<Person>();
-    all.add(new Person(new Person.Key("Bob"), 18));
-    schema.people().insert(all);
-    schema.commit();
-    schema.rollback();
-    List<Person> r = schema.people().olderThan(10).toList();
-    assertEquals(1, r.size());
-  }
-
-  @Test
-  public void testCommitTransaction() throws SQLException, OrmException {
-    PhoneBookDb schema = phoneBook.open();
-    schema.updateSchema(executor);
-    schema.people().beginTransaction(null);
-    ArrayList<Person> all = new ArrayList<>();
-    all.add(new Person(new Person.Key("Bob"), 18));
-    schema.people().insert(all);
-    schema.commit();
-    List<Person> r = schema.people().olderThan(10).toList();
-    assertEquals(1, r.size());
-  }
 }
