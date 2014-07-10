37514867443cee083b4e18ea8aa166b23a6d71a5
David Ostrovsky <david@ostrovsky.org>
2014-07-10 10:37:11 +0200

    Extract base test class
    Change-Id: I4409667887f7744ae782106474fc617ae40c41fd

---
+++ src/test/java/com/google/gwtorm/schema/sql/SqlDialectTest.java
@@ -0,0 +1,30 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gwtorm.schema.sql;
+
+import com.google.gwtorm.data.PhoneBookDb;
+import com.google.gwtorm.data.PhoneBookDb2;
+import com.google.gwtorm.jdbc.Database;
+import com.google.gwtorm.jdbc.JdbcExecutor;
+
+import java.sql.Connection;
+
+public abstract class SqlDialectTest {
+  protected JdbcExecutor executor;
+  protected Connection db;
+  protected SqlDialect dialect;
+  protected Database<PhoneBookDb> phoneBook;
+  protected Database<PhoneBookDb2> phoneBook2;
+}
