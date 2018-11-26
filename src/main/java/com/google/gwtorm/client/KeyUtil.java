a30f03fdfe736d2c48b76fa0208165620236021d
David Ostrovsky <david@ostrovsky.org>
2018-11-26 22:01:53 +0100

    KeyUtil: Initialize ENCODER_IMPL to StandardKeyEncoder
    ENCODER_IMPL initialization takes places in Database class, but gerrit
    isgoing to remove the dependency on this class. Move the initialization
    to the KeyUtil itself to simplify the development and maintenance for
    gerrit project.
    
    Change-Id: I4b1b8de6949a6cc94bd004e2f983bb50b0a3270a

--- src/main/java/com/google/gwtorm/client/KeyUtil.java
+++ src/main/java/com/google/gwtorm/client/KeyUtil.java
@@ -14,9 +14,11 @@
 
 package com.google.gwtorm.client;
 
+import com.google.gwtorm.server.StandardKeyEncoder;
+
 /** Common utility functions for {@link Key} implementors. */
 public class KeyUtil {
-  private static Encoder ENCODER_IMPL;
+  private static Encoder ENCODER_IMPL = new StandardKeyEncoder();
 
   /**
    * Set the encoder implementation to a valid implementation.
